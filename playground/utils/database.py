7088c3053b54cd4e81459d253104850600c5ee96
SungminSo <soos196@hanyang.ac.kr>
2020-12-27 14:45:46 +0900

    utils/database.py 추가

---
+++ playground/utils/database.py
@@ -0,0 +1,20 @@
+from ..models import Session
+
+
+def transaction():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:  # noqa : # noqa가 있는 행은 linter 프로그램에 의해 무시됨.
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+def orm_session():
+    session = Session()
+    yield session
+
+    session.close_all()
