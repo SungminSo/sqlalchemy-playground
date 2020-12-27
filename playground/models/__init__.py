8f71ef6a62f579fa5db2ffcbaf106157f238e5b9
SungminSo <soos196@hanyang.ac.kr>
2020-12-27 14:41:44 +0900

    create a session

--- playground/models/__init__.py
+++ playground/models/__init__.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 # use in-memory-only SQLite db
 engine = create_engine('sqlite:///:memory', echo=True)
@@ -10,3 +11,9 @@ engine = create_engine('sqlite:///:memory', echo=True)
 Base = declarative_base()
 # sqlalchemy에서는 Declarative를 이용해서 사용할 class를 생성하고 db table에 매핑한다.
 
+
+Session = sessionmaker(bind=engine)
+# engine이 아직 존재하지 않을 경우
+# Session = sessionmake()
+# engine 생성 이후
+# Session.configure(bind=engine)
